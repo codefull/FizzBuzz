8ad1a4aa50051ab9e67ad4e185be53c30cceb544
Ernesto Guevara Ramírez <eguevarar@gmail.com>
2019-10-16 15:41:03 -0400

    Project Files added

--- FizzBuzzTesting/FizzBuzzUnitTest.cs
+++ FizzBuzzTesting/FizzBuzzUnitTest.cs
@@ -8,9 +8,18 @@ namespace FizzBuzzTest
     [TestClass]
     public class FizzBuzzUnitTest
     {
+        FizzBuzzBL fb = new FizzBuzzBL();
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestFizz()
         {
+            Assert.IsTrue(fb.isFizz(3), "Failing Fizz Test");
+        }
+
+        [TestMethod]
+        public void TestBuzz()
+        {
+            Assert.IsTrue(fb.isBuzz(5), "Failing Buzz Test");
         }
     }
 }
