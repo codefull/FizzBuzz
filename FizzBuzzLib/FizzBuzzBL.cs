806e8e8d0bd58b63d2fe28b7bfff0e4fc9aa5da5
Ernesto Guevara Ramírez <eguevarar@gmail.com>
2019-10-16 15:29:34 -0400

    Adding project files

---
+++ FizzBuzzLib/FizzBuzzBL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzLib
+{
+    public class FizzBuzzBL
+    {
+        public bool isFizz(int n)
+        {
+            return false;
+        }
+
+        public bool isBuzz(int n)
+        {
+            return false;
+        }
+
+        public bool isNotFB(int n)
+        {
+            return false;
+        }
+
+        public bool isFizzBuzz(int n)
+        {
+            return false;
+        }
+    }
+}
